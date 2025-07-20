327717e202fa1db327e3eb958c475d60dcdeeb5e
Assita <adoucancy@gmail.com>
2025-07-21 00:27:19 +0200

    Connexion mise à jour avec JawsDB (Heroku)

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
+
 <?php
-// config.php : connexion à la base de données
-$host = 'localhost';
-$dbname = 'ecoride_db';
-$user = 'root';
-$pass = '';
+$host = "x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "i1pkw5txpfcxhlxd";
+$password = "o4c7inx2lzmghfu3";
+$dbname = "cb45a9wuvkj4mwn0";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    // Optionnelle : afficher les erreurs PDO (à désactiver en production)
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
