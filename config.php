992277672b8aa8212ff0147df4a5679bff3f8e4f
Assita <adoucancy@gmail.com>
2025-07-21 02:04:55 +0200

    Ecoride JawsDB

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 
 <?php
-$host = "x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user = "i1pkw5txpfcxhlxd";
-$password = "o4c7inx2lzmghfu3";
-$dbname = "cb45a9wuvkj4mwn0";
+$host = "x3zt8d54gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "ilpkwbx5vpfcnhlxd";
+$password = "o4cz7nlx21emghfu3";
+$dbname = "cb45a9wuvkj4mwv0";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f8d6badab5318ee762ba4a0e9d6faea092851a08',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f8d6badab5318ee762ba4a0e9d6faea092851a08',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
