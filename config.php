5a550c8dcb46bda6a1f6d284bfaeee33453e693a
Assita <adoucancy@gmail.com>
2025-09-29 11:58:21 +0200

    modication

--- config.php
+++ config.php
@@ -1,20 +1,4 @@
 <?php
-/*$db = parse_url(getenv("JAWSDB_URL"));
-
-$host = $db["host"];
-$user = $db["user"];
-$pass = $db["pass"];
-$db_name = ltrim($db["path"], "/");
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>
-*/
-
 
 $jawsdb_url = getenv("JAWSDB_URL");
 
